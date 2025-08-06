30df87903b02c9e5b11af772bde3a571712c288c
runwezt <amarikwat@gmail.com>
2025-08-06 01:56:49 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/web_restaurant/',
+  base: '/web_restaurant/', // 👈 important
 });
